9fffc28ed176f4a6b216ae2880779de529b94508
mike72park <46614173+mike72park@users.noreply.github.com>
2023-12-03 20:13:31 +0900

    Add root path route handler 2

--- app_db.js
+++ app_db.js
@@ -6,7 +6,7 @@ const { Member } = db;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('URL should contain /api/...');    
+    res.send('URL should contain /api/.../');    
 });
 
 app.get('/api/members', async (req, res) => {
